7a2826b6f093bb7061dafe0082e231942404baf5
bitcheck <kepivlde@gmail.com>
2021-01-06 17:46:49 +0800

    0.0.1

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -1,3 +1,3 @@
-use crate::{Error, mock::*};
-use frame_support::{assert_ok, assert_noop};
+// use crate::{Error, mock::*};
+// use frame_support::{assert_ok, assert_noop};
 
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,6 +41,8 @@ pub use frame_support::{
 /// Import the template pallet.
 pub use pallet_template;
 
+pub use pallet_poe;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -266,6 +268,10 @@ impl pallet_template::Trait for Runtime {
 	type Event = Event;
 }
 
+impl pallet_poe::Trait for Runtime {
+  type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -282,7 +288,8 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment::{Module, Storage},
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
-		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+    TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+    PoeModule: pallet_poe::{Module, Call, Storage, Event<T>},
 	}
 );
 
